fee0a5ccd3f58df74d6da8c309920f63fd47685c
AytenZeynalova <14rvxpg@code.edu.az>
2022-10-27 15:12:57 +0400

    task

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// // task1
+// let A=prompt("include number:");
+// for (let n = 1; n < 1000; n++) {
+//     if(n%A==0){
+//         console.log(n)
+//     }
+    
+// }
+
+
+
+
+// // task2
+// let B=prompt("include number:");
+
+
+// let count = 0
+// for (let x = 0; x*x < B; x++) {
+
+//     if(x>0){
+//         count ++;
+//     }
+// }
+// console.log(count);
+
+
+
+
+
+// // task3
+// let C = prompt("include number:")
+
+
+// let num=0;
+// for (let y = 1; y < C; y++) {
+//     if(C%y==0){ 
+//     num=y
+//     }
+// }
+// console.log(num)
+
+
+
