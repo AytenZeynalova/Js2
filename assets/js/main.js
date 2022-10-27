ff50c7a53327a2894c8dfa2cf462c2a9c9a31603
AytenZeynalova <14rvxpg@code.edu.az>
2022-10-27 17:34:57 +0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,4 +40,50 @@
 // console.log(num)
 
 
+// // task4
+// let K= prompt("include Number K:")
+// let F= prompt("include Number F:")
+// let sum=0;
+
+
+// if (K>F){
+//     for (let i = F; i < K; i++) {
+//         if(i%7==0){
+//             sum+=i;
+//         }
+//     }
+// }else if(F>K){
+//     for (let i = K; i < F; i++) {
+//         if(i%7==0){
+//             sum+=i;
+//         }
+//     }
+// }else{
+//     console.log("there is no such number here")
+// }
+
+// console.log(sum)
+
+
+// task5
+let n1=1;
+let n2=1;
+let nextnum;
+do{
+    let num1=+prompt("include fibonacci number :")
+}while(num1<0)
+
+for (let p = 1; p <= num1; p++) {
+    nextnum=n1+n2;
+    n1=n2;
+    n2=nextnum;
+}
+console.log(n1);
+
+// task6
+// let number1=+prompt("include number 1:")
+// let number2=+prompt("include number 2:")
+
+
+// task7
 
